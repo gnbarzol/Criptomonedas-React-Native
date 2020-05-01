88f1c9fdf3f867068097a936b21b73bdb573a112
Gary Barzola <gnbarzol@espol.edu.ec>
2020-05-01 15:03:55 -0500

    Se agrega una fuente externa y se crea el header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Text, Platform, StyleSheet } from 'react-native';
+import {Text, Platform, StyleSheet} from 'react-native';
 
 const Header = () => <Text style={styles.encabezado}>Criptomonedas</Text>;
 
 const styles = StyleSheet.create({
   encabezado: {
     paddingTop: Platform.OS === 'ios' ? 50 : 10,
+    fontFamily: 'Lato-Black', //Para agregar esta fuente se debe de crear el archivo react-native.config.js luego reinciar el proyecto, luego 'npx react-native link' para linkear la dependencia de la fuente y por ultimo, reiniciar el simulador.
+    backgroundColor: '#5E49E2',
+    paddingBottom: 10,
+    fontSize: 20,
+    color: 'white',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    marginBottom: 30,
   },
 });
 
